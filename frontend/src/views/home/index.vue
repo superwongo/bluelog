c59c6a59a4babf39ef0a58cb8524ffc94357f2de
Administrator <wangchao_198912@126.com>
2019-06-10 14:57:23 +0800

    前端模板优化

--- frontend/src/views/home/index.vue
+++ frontend/src/views/home/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="`Welcome to Your Vue.js App, ${this.getUserInfo.name}`"/>
   </div>
 </template>
 
 <script>
 
+import userMixin from '@/mixins/user-mixin'
+
 export default {
-  name: 'home'
+  name: 'home',
+  mixins: [userMixin]
 }
 </script>
